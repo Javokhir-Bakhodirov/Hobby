7a8bbfafcf959143eee589b1fbf9419d957126bf
javokhir3414@icloud.com <b35.97-102-222-29@Macbook-Air.local>
2024-01-19 20:08:04 +0500

    ver1

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let elQuestion1 = prompt("1. Ismingizni kiriting");
+let elQuestion2 = prompt("2. familiyangizni kiriting");
+let elQuestion3 = prompt("3. Yoshingizni kiriting ");
+let elQuestion4 = prompt("4. Hobiyingizni kiriting");
+let elQuestion5 = prompt("5. Ish joyingizni(Talim markazingizni) kiriting ");
+
+console.log("1. Ismingizni kiriting  - " + elQuestion1);
+console.log("2. familiyangizni kiriting - " + elQuestion2);
+console.log("3. Yoshingizni kiriting - " + elQuestion3);
+console.log("4. Hobiyingizni kiriting - " + elQuestion4);
+console.log("5. Ish joyingizni(Talim markazingizni) kiriting - " + elQuestion5);
